2b7f389cad337bd309a067346e39a9b5b71c9fca
sumi-re01 <owob.viola@gmail.com>
2021-03-08 06:29:12 +0000

    [Add]住所

--- Bookers2/app/controllers/users_controller.rb
+++ Bookers2/app/controllers/users_controller.rb
@@ -34,7 +34,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image,
+                                 :postcode, :prefecture_name, :address_city, :address_street, :address_building)
   end
 
 end
--- Bookers2/app/models/user.rb
+++ Bookers2/app/models/user.rb
@@ -12,4 +12,16 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   attachment :profile_image
+
+  # prefecture_code => prefecture_name
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
